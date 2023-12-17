aac86f32bb1e2822b1876c6bc02d039f718bf1d9
Arjun <iarjun.sahlot@gmail.com>
2023-12-17 13:04:26 -0800

    Update plantableJQB.js

--- plantableJQB.js
+++ plantableJQB.js
@@ -1,11 +1,15 @@
+console.log("STEP 1");
 if (typeof plantableJQB === 'undefined') var plantableJQB = {};
 if (typeof CCSE === 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
 plantableJQB.name = 'Plantable JQB';
 plantableJQB.version = '1.0.0';
 plantableJQB.GameVersion = '2.052';
+console.log("STEP 2");
 
 plantableJQB.launch = function() {
+    console.log("STEP 3");
     plantableJQB.init = function() {
+        console.log("STEP 4");
         let startupString = 'Juicy Queenbeet not found, couldn\'t be set to plantable.';
 
         // Ensure the modification is done after the Farm minigame is fully loaded
@@ -23,10 +27,12 @@ plantableJQB.launch = function() {
         }
         if (Game.prefs.popups) Game.Popup(startupString);
         else                   Game.Notify(startupString, '', '', 0, 1);
+        console.log("STEP 5");
     };
 };
 
 if (!plantableJQB.isLoaded) {
+    console.log("STEP 6");
     if (CCSE && CCSE.isLoaded) {
         plantableJQB.launch();
     } else {
@@ -35,3 +41,4 @@ if (!plantableJQB.isLoaded) {
         CCSE.postLoadHooks.push(plantableJQB.launch);
     }
 }
+console.log("STEP 7");
