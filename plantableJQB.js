59c46874896f5885af2b6e75e7620bf50e948f4f
Arjun <iarjun.sahlot@gmail.com>
2023-12-17 12:46:32 -0800

    Update plantableJQB.js

--- plantableJQB.js
+++ plantableJQB.js
@@ -1,24 +1,25 @@
 if (plantableJQB === undefined) var plantableJQB = {};
 if (typeof CCSE == "undefined") Game.LoadMod("https://klattmose.github.io/CookieClicker/CCSE.js");
+plantableJQB.name = "Plantable JQB";
+plantableJQB.version = "1.0.0";
+plantableJQB.GameVersion = "2.052";
 
 plantableJQB.launch = function() {
-	plantableJQB.name = "Plantable JQB";
-	plantableJQB.version = "1.0.0";
-	plantableJQB.GameVersion = "2.052";
+	plantableJQB.init = function () {
+		CCSE.MinigameReplacer(function() {
+			var M = Game.Objects["Farm"].minigame;
+			if (M.plants["queenbeetLump"]) {
+				M.plants["queenbeetLump"].plantable = true;
+				console.log("Juicy Queenbeet is now plantable!");
+			} else {
+				console.log("Juicy Queenbeet not found.");
+			}
+		}, "Farm");
 	
-	CCSE.MinigameReplacer(function() {
-		var M = Game.Objects["Farm"].minigame;
-		if (M.plants["queenbeetLump"]) {
-			M.plants["queenbeetLump"].plantable = true;
-			console.log("Juicy Queenbeet is now plantable!");
-		} else {
-			console.log("Juicy Queenbeet not found.");
+		plantableJQB.isLoaded = 1;
+		if (CCSE.ConfirmGameVersion(plantableJQB.name, plantableJQB.version, plantableJQB.GameVersion)) {
+			Game.registerMod(plantableJQB.name, plantableJQB);
 		}
-	}, "Farm");
-
-	plantableJQB.isLoaded = 1;
-	if (CCSE.ConfirmGameVersion(plantableJQB.name, plantableJQB.version, plantableJQB.GameVersion)) {
-		Game.registerMod(plantableJQB.name, plantableJQB);
 	}
 }
 
